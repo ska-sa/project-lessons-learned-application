a2cbe683148c2af763fe94f9429cd0263fb7bc53
Anele Basopu <anelebasopu@gmail.com>
2025-05-15 22:09:37 +0200

    Update store.ts

--- Frontend/src/store/store.ts
+++ Frontend/src/store/store.ts
@@ -2,13 +2,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import lessonsReducer from "../features/lessons/lessonsSlice";
-import projectsReducer from "../features/projects/projectsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     lessons: lessonsReducer,
-    projects: projectsReducer,
   },
 });
 
